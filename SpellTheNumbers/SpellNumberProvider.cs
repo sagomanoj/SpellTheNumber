652d4759b4342dcca82f81ab548a8d801e6bada1
sagomanoj <33486909+sagomanoj@users.noreply.github.com>
2020-06-07 17:30:47 +0530

    Add files via upload
    Added SpellNumber Provider

---
+++ SpellTheNumbers/SpellNumberProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpellTheNumbers
+{
+    public class SpellNumberProvider
+    {
+
+        private static SpellNumberProvider _instance = null;
+        private SpellNumberProvider()
+        {
+        }
+
+        public static SpellNumberProvider Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new SpellNumberProvider();
+
+                return _instance;
+            }
+        }
+    }
+}
