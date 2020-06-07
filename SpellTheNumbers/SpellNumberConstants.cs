f651590af6206394b921d071daf6f2ca3351c36a
sagomanoj <33486909+sagomanoj@users.noreply.github.com>
2020-06-07 17:25:32 +0530

    Add files via upload
    Added Spell Number Constant

---
+++ SpellTheNumbers/SpellNumberConstants.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpellTheNumbers
+{
+    static class SpellNumberConstants
+    {
+        public const string Hundred = "hundred";
+        public const string Thounsand = "thounsand";
+        public const string Million = "million";
+        public const string Trillion = "trillion";
+        public const string Billion = "billion";
+        public const string Quadrillion = "quadrillion";
+        public const string Quintillion = "quintillion";
+        public const string Zero = "zero";
+
+
+        public const long OneQuintillion = 1000000000000000000;
+        public const long OneQuadrillion = 1000000000000000;
+        public const long OneTrillion = 1000000000000;
+        public const long OneBillion = 1000000000;
+        public const long OneMillion = 1000000;
+        public const long OneThounsand = 1000;
+        public const long OneHundred = 100;
+        public const long Twenty = 20;
+        public const long Ten = 10;
+    }
+}
